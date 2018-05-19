8548cb6ed416c3cfa965dba2480053e65d74e4ef
kog-7 <443536402@qq.com>
2018-05-20 00:26:20 +0800

    'build'

---
+++ index.js
@@ -0,0 +1,31 @@
+let cwd=process.cwd();
+let nodepath=require('path');
+
+let readConfig=(cwd)=>{
+  let ph=false;
+  try{
+    ph=require(nodepath.join(cwd,'map-path.js'));
+  }
+  catch(e){
+    throw `map-path.js is not exits in project root path,use module.exports={name:real path}`;
+  }
+  return ph;
+};
+
+let mapPath=(store=null)=>(cwd)=>(ph)=>{
+  if(store===null){
+    store=readConfig(cwd);
+
+    if(!(typeof store==='object')){
+      throw `map-path.js must use format like module.exports={name:real path}`;
+    }
+  }
+  if(!(ph in store)){
+    throw `${ph} is not map in map-path.js`;
+  }
+  return require(store[ph]);
+};
+
+
+
+module.exports=mapPath(null)(cwd);
---
+++ readme.md
@@ -0,0 +1,22 @@
+#   map-path   #
+
+require map path
+
+![](https://img.shields.io/npm/v/map-path.svg?style=flat)
+
+
+## useage
+
+// use utils
+
+```js  
+let utils=require('map-path')('Utils');
+```
+
+// map path in map-path.js which in the root dir
+```js
+const nodepath=require('path');
+module.exports={
+  'Utils':'./src/utils.js'
+}
+```

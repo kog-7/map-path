f771a2175fb70ea2b3b4511ff9eb0f45c28f9152
kog-7 <443536402@qq.com>
2018-11-04 12:03:24 +0800

    morepath

--- index.js
+++ index.js
@@ -1,44 +1,59 @@
-let cwd=process.cwd();
-let nodepath=require('path');
+let nodepath = require("path");
 
-let readConfig=(cwd)=>{
-  let ph=false;
-  try{
-    ph=require(nodepath.join(cwd,'map-path.js'));
-  }
-  catch(e){
-    throw `map-path.js is not exits in project root path,use module.exports={name:real path}`;
+let store = null;
+let cwd = process.cwd();
+
+let readConfig = () => {
+  let ph = false;
+  try {
+    ph = require(nodepath.join(cwd, "map-path.js"));
+  } catch (e) {
+    throw e;
   }
   return ph;
 };
 
-let mapPath=(store=null)=>(cwd)=>{
-
-  return (ph,type)=>{
-
-    if(store===null){
-      store=readConfig(cwd);
-
-      if(!(typeof store==='object')){
-        throw `map-path.js must use format like module.exports={name:real path}`;
-      }
-    }
-    if(!(ph in store)){
-      throw `${ph} is not map in map-path.js`;
-    }
-
-    if(type==='url'){
-      return store[ph];
-    }
-    else{
-      return require(store[ph]);
-    }
-
-
+let addJsPrefix = path => {
+  let last = path.lastIndexOf("/");
+  last = last === -1 ? 0 : last;
+  let ph = path.slice(last);
+  if (ph.indexOf(".") === -1) {
+    //no .file
+    path = path + "/index.js";
   }
-
+  return path;
 };
 
+let mapPath = (ph, type) => {
+  if (store === null) {
+    store = readConfig();
+    if (!(typeof store === "object")) {
+      throw `map-path.js must use format like module.exports={name:real path}`;
+    }
+  }
+  let phArr = ph.split("/");
+  phArr = phArr.map(p => {
+    if (p) {
+      return p;
+    } else {
+      return false;
+    }
+  });
+  let firstPath = phArr.shift();
+  if (!(firstPath in store)) {
+    throw `${firstPath} is not map in map-path.js`;
+  }
 
+  let outPath = nodepath.join(store[firstPath], phArr.join("/"));
+  if (type === "url") {
+    return outPath;
+  } else {
+    return require(addJsPrefix(outPath));
+  }
+};
 
-module.exports=mapPath(null)(cwd);
+mapPath.setRoot = url => {
+  cwd = url;
+};
+
+module.exports = mapPath;
--- readme.md
+++ readme.md
@@ -1,24 +1,31 @@
-#   map-path   #
+# map-path
 
 require map path
 
-
 ![](https://img.shields.io/npm/v/map-path.svg?style=flat)
-
 
 ## useage
 
 // use utils
 
-```js  
-let utils=require('map-path')('Utils');
-let utilsUrl=require('map-path')('Utils','url');
+```js
+let utils = require("map-path")("Utils");
+let utilsUrl = require("map-path")("Utils", "url");
+let Button = require("map-path")("Components/Button", "url"); //default ..../Button/index.js
 ```
 
 // map path in map-path.js which in the root dir
+
 ```js
-const nodepath=require('path');
-module.exports={
-  'Utils':nodepath.join(__dirname,'./src/utils.js')
-}
+const nodepath = require("path");
+module.exports = {
+  Utils: nodepath.join(cwd, "utils"),
+  Components: nodepath.join(cwd, "components")
+};
+```
+
+//if not in rootPath
+
+```
+require("map-path").setRoot('./env');
 ```
